3a8d3b41478d0f466b74b7c36ef9fb248d21f06b
Jairo411 <jairoalbertomorelli@gmail.com>
2024-08-22 08:54:10 -0400

    Added module locally, changing project to full front end web application
    I manage to add a module to my javascript project locally. By
    literally downloading the module and adding to it to my project.
    I believe that because this is actually pretty far to how the industry
    actually creates web applications that I am going to change my
    project to resemble no matter how little to an actual web application.
    
    This is me just currently saving my progress, I expect that everything
    already is pretty broken.

--- res/scripts/script.js
+++ res/scripts/script.js
@@ -1,3 +1,10 @@
+import * as threejsMath from "../scripts/threejs/src/index.js";
+
+//GLOBAL VARIABLES
+let mouseCoords= {};
+mouseCoords.pos= threejsMath.Vector2(0,0);
+
+
 ///CANVAS VARIABLES
 let canvas;
 let context;
@@ -5,33 +12,52 @@ let context;
 
 ///GAME LOOP VARIALES
 let isrunning=false;
-let secondsPassed;
+let secondsPassed;  
 let oldTimeStamp = 0.0;
 let fps;
 ///GAME LOOP VARIABLES
 
-///GAMEOBJECT OBJECT VARIABLE
-function gameObject(id,image)
-{
-    this.id=id;
-    this.image=image;
-    // Transformation matrix stored in a 2d array 
-// [[a c e],
-//  [b d f],
-//  [0 0 1]
-    this.transform= [[0,0,0],[0,0,0],[0,0,0]];
+
+class gameObject{
+    id=undefined;
+    image=new Image();
+    #transform = new threejsMath.Matrix3();
+    constructor(id,image){
+        this.id=id;
+        this.image=image;
+        this.#transform.set(0,0,0,0,0,0,0,0,0,0);
+    }
 }
-///GAMEOBJECT OBJECT VARIABLE
-///GAMEOBJECT REFERENCE VARIABLES
-let rockObj;
-let scissorObj;
-let paperObj;
+
+class UI_square{
+    constructor(){
+        this.x=0;
+        this.y=0;
+        this.width=0;
+        this.height=0;
+        this.offsetx=0;
+        this.offsety=0;
+    }
+    returnBoundingbox()
+    {
+        let BB={};
+        BB.minX=0;
+        BB.minY=0;
+        BB.maxX=0;
+        BB.maxY=0;
+
+        BB.minX=this.UI_square.x;
+        BB.minY=this.UI_square.y;
+        BB.maxX=this.UI_square.x+this.width;
+        BB.MaxY=this.UI_square.y+this.height;
+    }
+}
+
+
 ///GAMEOBJECT REFERENCE VARIABLES
 ///ENTITY HOLDER///
 let EntityHolder=[];
 
-//UI Object 
-let UI_square= {};
 
 UI_square.x=0;
 UI_square.y=0;
@@ -39,11 +65,51 @@ UI_square.width=0;
 UI_square.height=0;
 UI_square.offsetx=0;
 UI_square.offsety=0;
+UI_square.returnBoundingbox = function()
+{
+    let BB={};
+    BB.minX=0;
+    BB.minY=0;
+    BB.maxX=0;
+    BB.maxY=0;
 
-//UI_square.DrawUIImage= funcion(UI_image)
-//{
+    BB.minX=this.UI_square.x;
+    BB.minY=this.UI_square.y;
+    BB.maxX=this.UI_square.x+this.width;
+    BB.maxY=this.UI_square.y+this.height;
+}
+UI_square.collisionDetection= function(point, align_bounding_box)
+{
+    ///Get point turn it into a vec2 
+    let pVec2=[point.x,point.y];
+    // Get bounding box position, turn it into a vec2
+    let bVec2=[align_bounding_box.minX,align_bounding_box.minY,align_bounding_box.maxX,align_bounding_box.maxY];
 
-//}
+
+    let qVec2=[0,0];
+    //Get clamped Q on B to P
+    for(i = 0; i< 2; i++)
+    {
+        let v = pVec2[i];
+        if(v < bVec2.minX){
+            v = bVec2.minX
+        }else if (v <bVec2.minY){
+            v= bVec2.minY;
+        }
+
+        if(v > bVec2.maxX){
+            v = bVec2.maxX
+        }else if(v  > bVec2.maxY){
+            v = bVec2.maxY;
+        }
+        qVec2[i]=v;
+    }
+
+    //Now that I have P to the bounds of B, which is now called Q, I can compare both P Q. 
+    //If the magnitude of both P and Q is smaller then the distance between the points, a collision is detected.
+
+    
+};
 //UI Object constructor 
 function CUI_square(x_,y_,width_,height_,offsetX_,offsetY_)
 {
@@ -69,10 +135,16 @@ function DrawUISquare(UI_square)
 const Scene0 = {
     CreateScene(){
 
-        let paperimg = new Image();
+
+        let rockObj=new gameObject(0,new Image());
+
+
+
+        let paperimg = new Image(800,800)
         let scissorimg= new Image();
         let rockimg = new Image();
-    
+
+        
         paperimg.src="res/images/paper.png";
         scissorimg.src="res/images/scissors.png";
         rockimg.src="res/images/rock.png";
@@ -110,6 +182,9 @@ const Scene0 = {
         EntityHolder.push(scissorObj)
 
 
+        const a = new threejsMath.Vector2(2,2);
+        console.log(a);
+
     },
     DrawScene(){
         for(var i = 0; i<= EntityHolder.length-1; i++)
@@ -119,15 +194,43 @@ const Scene0 = {
             
             }
     },
-    DrawUI(){
+    UI(){
 
         let UI_S = new CUI_square (240,500,150,150);
         let UI_S1= new CUI_square (440,500,150,150);
         let UI_S2= new CUI_square (640,500,150,150);
 
+
+        let UI_Manager=[UI_S,UI_S1,UI_S2];
+
+        let smallpaper= new Image();
+        smallpaper.src="res/images/smallpaper.png";
+        let smallrock = new Image();
+        smallrock.src="res/images/smallrock.png";
+        let smallscissor= new Image();
+        smallscissor.src="res/images/smallscissors.png";
+
         DrawUISquare(UI_S);
+      
+    
+        context.drawImage(smallpaper,276,530);
+    
         DrawUISquare(UI_S1);
+
+        context.drawImage(smallrock,476,530);
+    
+
         DrawUISquare(UI_S2);
+        context.drawImage(smallscissor,674,530);
+
+
+
+
+         //  for(i=0; i<UI_Manager.length; i++){
+          //      UI_Manager[i].collisionDetection(mouseCoords,UI_Manager[i].returnBoundingbox);
+          // };
+        
+          
 
     },
     gameScene()
@@ -207,7 +310,7 @@ function gameloop(timeStamp)
     //calculateGameLoop(performance.now());
     Scene0.gameScene()
     Scene0.DrawScene();
-    Scene0.DrawUI();
+    Scene0.UI();
     Scene0.ClearScene();
     if(isrunning==true){
  //  window.requestAnimationFrame(gameloop);
@@ -223,7 +326,9 @@ function gameloop(timeStamp)
 intialization();
 
 canvas.addEventListener("click",(e)=>{
-    console.log("X and Y position of Mouse click:" + e.clientX + "," + e.clientY);
+    mouseCoords.pos.x=e.clientX;
+    mouseCoords.pos.y=e.clientY;
+    console.log("X and Y position of Mouse click:" + mouseCoords.pos.x + "," + mouseCoords.pos.y);
 });
 
 /* you don't even need to add this*/
