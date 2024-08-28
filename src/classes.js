cea4276249ceeb37838a1b39f7953c145b788255
Jairo411 <jairoalbertomorelli@gmail.com>
2024-08-28 15:55:50 -0400

    Turning MY Project into a Frontend Web Application

--- src/classes.js
+++ src/classes.js
@@ -1,2 +1,62 @@
+const threeJsMod= require("threejs-math");
+class gameObject{
+    constructor(id_,Image_){
+        this.id= id_;
+        this.Image= Image_;
+        this.transform= new threeJsMod.Matrix3();
+        this.x=0;
+        this.y=0;
+        this.transform.set(0,0,this.x,0,0,this.y,0,0,0);
+    }
+
+}
+class UI_Square{
+    constructor(x_,y_,width_,height_,offSetX_,offSetY_)
+    {
+        this.x=x_;
+        this.y=y_;
+        this.width=width_;
+        this.height=height_;
+        this.offSetX=offSetX_;
+        this.offSetY=offSetY_;
+    }
+    
+
+    collisionDectection()
+    {
+         ///Get point turn it into a vec2 
+    let pVec2=[point.x,point.y];
+    // Get bounding box position, turn it into a vec2
+    let bVec2=[align_bounding_box.minX,align_bounding_box.minY,align_bounding_box.maxX,align_bounding_box.maxY];
 
 
+    let qVec2=[0,0];
+    //Get clamped Q on B to P
+    for(i = 0; i< 2; i++)
+    {
+        let v = pVec2[i];
+        if(v < bVec2.minX){
+            v = bVec2.minX
+        }else if (v <bVec2.minY){
+            v= bVec2.minY;
+        }
+
+        if(v > bVec2.maxX){
+            v = bVec2.maxX
+        }else if(v  > bVec2.maxY){
+            v = bVec2.maxY;
+        }
+        qVec2[i]=v;
+    }
+}
+
+    ReturnBoundingBox()
+    {
+        
+    }
+}
+
+
+exports.gameObject= gameObject;
+exports.UI_Square=UI_Square;
+exports.threeJsMod=threeJsMod;
---
+++ src/main-script.js
@@ -0,0 +1,200 @@
+const modules = require("./classes");
+
+//GLOBAL VARIABLES
+let mouseCoords= {};
+mouseCoords.pos= new modules.threeJsMod.Vector2(0,0);
+///CANVAS VARIABLES
+let canvas;
+let context;    
+///CANVAS VARIABLES
+
+///GAME LOOP VARIALES
+let isrunning=false;
+let secondsPassed;  
+let oldTimeStamp = 0.0;
+let fps;
+///GAME LOOP VARIABLES
+
+///GAMEOBJECT REFERENCE VARIABLES
+///ENTITY HOLDER///
+let EntityHolder=[];
+//GLOBAL VARIABLES
+
+const Scene0 = {
+    CreateScene(){
+        
+
+        let rockObj=new modules.gameObject(0,new Image());
+        let scissorObj= new modules.gameObject(1,new Image());
+        let paperObj = new modules.gameObject(2, new Image());
+
+        let paperimg = new Image(800,800)
+        let scissorimg= new Image();
+        let rockimg = new Image();
+
+        
+        paperimg.src="../public/images/paper.png";
+        scissorimg.src="../public/images/scissors.png";
+        rockimg.src="../public/images/rock.png";
+    
+    
+        rockObj = new modules.gameObject();
+        rockObj.transform.translate(-600,200);
+    
+        scissorObj= new modules.gameObject();
+        scissorObj.transform.translate(0,-200);
+    
+        paperObj = new modules.gameObject();
+    
+        //intializing rock
+        rockObj.id=0;
+        rockObj.image=rockimg;
+        
+        //intializing scissors 
+        scissorObj.id=1;
+        scissorObj.image=scissorimg;
+        
+        //intializing paper 
+        paperObj.id=2;
+        paperObj.image=paperimg;
+    
+    
+        EntityHolder.push(rockObj);
+        EntityHolder.push(scissorObj)
+        EntityHolder.push(paperObj);
+
+
+    },
+    DrawScene(){
+        for(let i=0; i> EntityHolder.length; i++){
+            DrawSprite1(EntityHolder[i].Image,EntityHolder[i].x,EntityHolder[i].y);
+        }
+    },
+    UI(){
+
+        let UI_S = new modules.UI_Square (240,500,150,150);
+        let UI_S1= new modules.UI_Square (440,500,150,150);
+        let UI_S2= new modules.UI_Square (640,500,150,150);
+
+
+        //Creating UI objects that use, the rock, paper, scissors assets 
+        //They may appear to be the same like my gameObject objects, but 
+        //they're not, they peform and serve different functions.
+        let UI_Manager=[UI_S,UI_S1,UI_S2];
+        let smallpaper= new Image();
+        smallpaper.src="../public/images/smallpaper.png";
+        let smallrock = new Image();
+        smallrock.src=="../public/images/smallrock.png";
+        let smallscissor= new Image();
+        smallscissor.src=="../public/images/smallscissors.png";
+
+        DrawUISquare(UI_S);
+      
+    
+        context.drawImage(smallpaper,276,530);
+    
+        DrawUISquare(UI_S1);
+
+        context.drawImage(smallrock,476,530);
+    
+
+        DrawUISquare(UI_S2);
+        context.drawImage(smallscissor,674,530);
+
+          
+
+    },
+    gameScene()
+    {
+    },
+    ClearScene()
+    {
+
+    },
+};
+
+//Scene Drawer helper functions
+
+   function DrawSprite0(CanvasImageSource_,x_source,y_source,h_source, x_destination,y_destination, w_destination, h_destination)
+    {
+        context.drawImage(CanvasImageSource_,x_source,y_source,w_source,h_source, x_destination, y_destination, w_destination,h_destination);
+    }
+
+   function DrawSprite1(CanvasImageSource_, x_source,y_source)
+    {
+        context.drawImage(CanvasImageSource_,x_source,y_source);
+    }
+
+   function DrawUISquare(UI_square)
+    {
+    context.fillRect(UI_square.x,UI_square.y,UI_square.width,UI_square.height);
+    //Hardcoded offset leaving UI_square with offset variable just in case I want to do cool stuff with it.
+    context.clearRect(UI_square.x+5,UI_square.y+5,UI_square.width-10,UI_square.height-10);
+    }
+
+function intialization()
+{
+    canvas = document.getElementById("viewport");
+    context = canvas.getContext("2d");
+
+    isrunning=true;
+    Scene0.CreateScene();
+    
+    if(isrunning===true){
+        //in order to use this properly I need to calculate the speed at which the canvas is running, convert it to miliseconds then divide it by my desire frame rate
+        // the units of time here are in milliseconds
+        //Creating a 16 ms delay on purpose
+    window.setInterval(gameloop,1000/60);
+    }
+}
+
+
+function calculateGameLoop(timeStamp)
+{
+    //I think that when the first time this gets excuted timestamp equals originally zero
+    //In order lock the frame rate, what you need is to make it so that the frame rate equals the same amount of executions in a second
+    secondsPassed= (timeStamp - oldTimeStamp) / 1000;
+    oldTimeStamp = timeStamp;
+
+    //calculate fps 
+    fps = Math.round(1/secondsPassed);
+
+    //Draw number to the screen 
+    context.fillStyle = 'white';
+    context.fillRect(930,0,200,100);
+    context.font = '25px Arial';
+    context.fillStyle = 'black';
+    context.fillText("FPS: " + fps, 930,30);
+}
+
+
+function gameloop(timeStamp)
+{
+    //calculateGameLoop(performance.now());
+    Scene0.gameScene()
+    Scene0.DrawScene();
+    Scene0.UI();
+    Scene0.ClearScene();
+    if(isrunning==true){
+ //  window.requestAnimationFrame(gameloop);
+     window.setInterval(gameloop,1000/60);
+    }
+    /*request animation frame will actually set the frame rate of my canvas 
+    there is no need to actually get the steps of my web browser and then use those 
+    steps to simulate my game logic.
+     */
+    
+}
+
+intialization();
+
+canvas.addEventListener("click",(e)=>{
+    mouseCoords.pos.x=e.clientX;
+    mouseCoords.pos.y=e.clientY;
+    console.log("X and Y position of Mouse click:" + mouseCoords.pos.x + "," + mouseCoords.pos.y);
+});
+
+/* you don't even need to add this*/
+document.addEventListener("keyup",(e)=>{
+    console.log(e.key+":button was pressed");
+});
